71bd2b576a37ca2e64cff04b2470176009ae433d
AndresRenteria <AndresRenteria@users.noreply.github.com>
2016-04-06 01:05:23 -0500

    Update mark.js

--- mark.js
+++ mark.js
@@ -2,43 +2,53 @@
 	{
 		"x":20.734280,
 		"y": -103.452811,
-		"title":"food1"
+		"title":"food1",
+		"desc":"this is the restaurant food1 where we serve all types of foods1"
 	},
 	{
 		"x":20.734602,
 		"y": -103.452754,
-		"title":"food2"
+		"title":"food2",
+		"desc":"this is the restaurant food2 where we serve all types of foods2"
 	},	{
 		"x":20.735031,
 		"y":-103.455688,
-		"title":"food3"
+		"title":"food3",
+		"desc":"this is the restaurant food3 where we serve all types of foods3"
 	},	{
 		"x":20.734999,
 		"y":-103.456013,
-		"title":"food4"
+		"title":"food4",
+		"desc":"this is the restaurant food4 where we serve all types of foods4"
 	},	{
 		"x":20.734969,
 		"y":-103.456431,
-		"title":"food5"
+		"title":"food5",
+		"desc":"this is the restaurant food5 where we serve all types of foods5"
 	},	{
 		"x":20.734588, 
 		"y":-103.457246,
-		"title":"food6"
+		"title":"food6",
+		"desc":"this is the restaurant food6 where we serve all types of foods6"
 	},	{
 		"x":20.734528,
 		"y":-103.457536,
-		"title":"food7"
+		"title":"food7",
+		"desc":"this is the restaurant food7 where we serve all types of foods7"
 	},	{
 		"x":20.733214,
 		"y":-103.455326,
-		"title":"food8"
+		"title":"food8",
+		"desc":"this is the restaurant food8 where we serve all types of foods8"
 	},	{
 		"x":20.733164,
 		"y":-103.454725,
-		"title":"food9"
+		"title":"food9",
+		"desc":"this is the restaurant food9 where we serve all types of foods9"
 	},	{
 		"x":20.736080,
 		"y":-103.455167,
-		"title":"food10"
+		"title":"food10",
+		"desc":"this is the restaurant food10 where we serve all types of foods10"
 	}
 ]
