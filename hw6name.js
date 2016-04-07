9970aa3bb75eb1f231267160572751ea72561bd9
AndresRenteria <AndresRenteria@users.noreply.github.com>
2016-04-06 19:43:44 -0500

    Create hw6name.js

---
+++ hw6name.js
@@ -0,0 +1,47 @@
+{
+  "friends": [
+      
+	{
+		"name":"Andres",
+		"hobby":"Videojuegos",
+		"age":"22",
+		"phone":"354926548",
+		"address":"Ramon Corona"
+	},
+	{
+		"name":"Enciso",
+		"hobby":"Salvar el mundo",
+		"age":"21",
+		"phone":"5452652333365",
+		"address":"Paseo de las colinas"
+	},
+	{
+		"name":"Diego",
+		"hobby":"Jugar computadora",
+		"age":"23",
+		"phone":"684238655256",
+		"address":"Santa Margarita"
+	},
+	{
+		"name":"Alvaro",
+		"hobby":"Videojuegos",
+		"age":"30",
+		"phone":"88456362344",
+		"address":"Hidalgo"
+	},
+	{
+		"name":"Gino",
+		"hobby":"Leer libros",
+		"age":"21",
+		"phone":"245458451358",
+		"address":"Inglaterra"
+	},
+		{
+		"name":"Eddy",
+		"hobby":"Programar edison",
+		"age":"25",
+		"phone":"87546248621",
+		"address":"Guadalajara"
+	}
+  ]
+}
